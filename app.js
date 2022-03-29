914f00c6bcff62aa6530122bc3f742f906c865e4
edrsjunior <102622957+edrsjunior@users.noreply.github.com>
2022-03-29 16:39:28 -0300

    Error port listen

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require("express"); /* IMPORTA O EXPRESS*/ 
-
 var app = express(); //START EXPRESS
+var PORT = 3003;
 
-app.listen(process.env.PORT || 3003,()=>{                                   //COLOCA O APP PARA ESCUTAR NA PORTA 3003 E
-    console.log("Server running on port 3003");         //FAZ UM CALLBACK RETORNADO RODANDO EM....
+
+app.listen(process.env.PORT || PORT,function(err){  
+    if (err) console.log(err);                                 //COLOCA O APP PARA ESCUTAR NA PORTA 3003 E
+    console.log("Server running on port ",PORT);         //FAZ UM CALLBACK RETORNADO RODANDO EM....
 });
 
 app.get("/food",(req, res)=>{                      //DEFINE UMA RESPOSTA PARA GET
