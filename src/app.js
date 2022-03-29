d815f416b50f2fa9f57e96b49051b51e8505daff
edrsjunior <102622957+edrsjunior@users.noreply.github.com>
2022-03-29 16:53:11 -0300

    Use body parser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require("express"); /* IMPORTA O EXPRESS*/ 
+const bodyParser = require("body-parser"); /* IMPORTA O BODY PARSER*/ 
+const { use } = require("express/lib/application");
 
-var app = express(); //START EXPRESS
+const app = express(); //START express
+
 var PORT = 3003;
 
+app.use(bodyParser.json()); //API ENTEDER REQ json
+app;use(bodyParser.urlencoded({extended: false})); //API ENTENDER PARAMETROS VIA URL
 
 app.listen(process.env.PORT || PORT,function(err){  
     if (err) console.log(err);                                 //COLOCA O APP PARA ESCUTAR NA PORTA 3003 E
