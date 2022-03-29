c4e0a59d5001b0b9d5e3ee2efb89e2ed8b954e0a
edrsjunior <102622957+edrsjunior@users.noreply.github.com>
2022-03-29 16:54:20 -0300

    aspas duplas to simples

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require("express"); /* IMPORTA O EXPRESS*/ 
-const bodyParser = require("body-parser"); /* IMPORTA O BODY PARSER*/ 
-const { use } = require("express/lib/application");
+const express = require('express'); /* IMPORTA O EXPRESS*/ 
+const bodyParser = require('body-parser'); /* IMPORTA O BODY PARSER*/ 
+const { use } = require('express/lib/application');
 
 const app = express(); //START express
 
@@ -11,9 +11,9 @@ app;use(bodyParser.urlencoded({extended: false})); //API ENTENDER PARAMETROS VIA
 
 app.listen(process.env.PORT || PORT,function(err){  
     if (err) console.log(err);                                 //COLOCA O APP PARA ESCUTAR NA PORTA 3003 E
-    console.log("Server running on port ",PORT);         //FAZ UM CALLBACK RETORNADO RODANDO EM....
+    console.log('Server running on port ',PORT);         //FAZ UM CALLBACK RETORNADO RODANDO EM....
 });
 
-app.get("/food",(req, res)=>{                      //DEFINE UMA RESPOSTA PARA GET
-    res.json(["Arroz", "feijão", "bife"]);              //req = request body | informações sobre o request
+app.get('/food',(req, res)=>{                      //DEFINE UMA RESPOSTA PARA GET
+    res.json(['Arroz', 'feijão', 'bife']);              //req = request body | informações sobre o request
 });                                                     //res = response body | informações
