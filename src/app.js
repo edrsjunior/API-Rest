2ed84843c9a13f5ec5e46756b42febc9ea9ae34c
edrsjunior <edilsonrsjunior@gmail.com>
2022-04-05 10:32:37 -0300

    register

--- src/app.js
+++ src/app.js
@@ -38,13 +38,31 @@ app.get('/pessoas/:pessoa_id',(req,res)=>{
     return res.status(200).json(onePessoaGet);
 });
 
-app.get('/pessoas',(req,res)=>{
-    return res.status(200).json(userDB);
+app.post('/register', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (!foundUser) {
+    
+            let hashPassword = await bcrypt.hash(req.body.password, 10);
+    
+            let newUser = {
+                id: Date.now(),
+                username: req.body.username,
+                email: req.body.email,
+                password: hashPassword,
+            };
+            users.push(newUser);
+            console.log('User list', users);
+    
+            res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
+        } else {
+            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
 });
 
-app.get('/food',(req, res)=>{                      //DEFINE UMA RESPOSTA PARA GET
-    res.json(['Arroz', 'feijão', 'bife']);              //req = request body | informações sobre o request
-});                                                     //res = response body | informações
 
 app.post('/login',async(req,res)=>{
     try{
