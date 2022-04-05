7091a1148c7584af8edc096cea73943c2b8828b9
edrsjunior <edilsonrsjunior@gmail.com>
2022-04-05 10:31:24 -0300

    login

--- src/app.js
+++ src/app.js
@@ -46,6 +46,30 @@ app.get('/food',(req, res)=>{                      //DEFINE UMA RESPOSTA PARA GE
     res.json(['Arroz', 'feijão', 'bife']);              //req = request body | informações sobre o request
 });                                                     //res = response body | informações
 
-app.post('/login',(req,res)=>{
-
+app.post('/login',async(req,res)=>{
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (foundUser) {
+    
+            let submittedPass = req.body.password; 
+            let storedPass = foundUser.password; 
+    
+            const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+            if (passwordMatch) {
+                let usrname = foundUser.username;
+                res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+            } else {
+                res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
+            }
+        }
+        else {
+    
+            let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+            await bcrypt.compare(req.body.password, fakePass);
+    
+            res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
 });
