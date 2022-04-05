b2978bc71dc11e9a679c77259a063ac8afcd3084
edrsjunior <edilsonrsjunior@gmail.com>
2022-04-05 10:02:33 -0300

    change pessoas to userDB

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express = require('express'); /* IMPORTA O EXPRESS*/ 
 const bodyParser = require('body-parser'); /* IMPORTA O BODY PARSER*/ 
+const session = require('express-session'); /*TIPO UMA SESSAO PHP*/ 
 
+const PORT = 3003;
 
 const app = express(); //START express
 app.use(express.json()); //USAR JSON
 
 //SIMULAR UM BD
-let pessoas = [];
+let userDB = [];
 
-var PORT = 3003;
+
 
 app.use(bodyParser.json()); //API ENTEDER REQ json
 app.use(bodyParser.urlencoded({extended: false})); //API ENTENDER PARAMETROS VIA URL
+app.use(session({secret:'asgfagag456as47g98a'})); //SEGREDO DA SENHA, PARAMETRO BASE
 
 app.listen(process.env.PORT || PORT,function(err){  
     if (err) console.log(err);                                 //COLOCA O APP PARA ESCUTAR NA PORTA 3003 E
@@ -25,24 +28,26 @@ app.get('/',(req,res)=>{
 app.post('/pessoas',(req,res)=>{
     const {id, nome, cpf,dataNasc} = req.body;
     const pessoa = {id,nome,cpf,dataNasc};
-    pessoas.push(pessoa);
-    return res.status(201).json(pessoas);
+    userDB.push(pessoa);
+    return res.status(201).json(userDB);
 
 });
 
 app.get('/pessoas/:pessoa_id',(req,res)=>{
     const {pessoa_id} = req.params;
-    const onePessoaGet = pessoas.find((pessoa)=>pessoa.id === pessoa_id);
+    const onePessoaGet = userDB.find((pessoa)=>pessoa.id === pessoa_id);
 
     return res.status(200).json(onePessoaGet);
 });
 
 app.get('/pessoas',(req,res)=>{
-    return res.status(200).json(pessoas);
+    return res.status(200).json(userDB);
 });
 
 app.get('/food',(req, res)=>{                      //DEFINE UMA RESPOSTA PARA GET
     res.json(['Arroz', 'feijão', 'bife']);              //req = request body | informações sobre o request
 });                                                     //res = response body | informações
 
-app.post()
+app.post('/login',(req,res)=>{
+
+});
